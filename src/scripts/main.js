f6e2888ac70e6b352d4e99f1a47eca21bfcb79fc
Nastya Humeniuk <perevalova43@icloud.com>
2025-05-10 19:07:25 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImge = document.getElementById('largeImg');
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', () => {
+  event.preventDefault();
+
+  const target = event.target;
+
+  if (target.tagName === 'IMG') {
+    const link = target.closest('a');
+
+    if (link) {
+      largeImge.src = link.href;
+    }
+  }
+});
